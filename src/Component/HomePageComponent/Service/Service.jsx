5aafdca63ade21ac32a35d03a268654cf568735c
AmdadulHQ <rimonamdadul301@gmail.com>
2024-05-02 22:31:56 +0600

    adding about section & service section

--- src/Component/HomePageComponent/Service/Service.jsx
+++ src/Component/HomePageComponent/Service/Service.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+
 
 const Service = () => {
+    const {data} = useQuery({
+        queryKey:['service'],
+        queryFn:async ()=> {
+             const res = await fetch('/service.json')
+             return res.json()
+        }
+    })
     return (
-        <section>
-            
+        <section className='my-20 max-w-screen-xl mx-auto'>
+           <div className="space-y-4">
+                <p className='text-orange text-xl font-bold text-center'>Service</p>
+                <h1 className='text-5xl text-center text-bigText font-bold'>Our Service Area</h1>
+                <p className="text-smText text-base font-normal max-w-[720px] mx-auto text-center">The majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+           </div>
+           <div className="grid grid-cols-3 gap-6">
+                {
+                    data && data.map(item => <div key={item.id} className="border p-6 rounded space-y-3">
+                        <img className="h-[208px] w-full rounded-lg" src={item.img} alt="" />
+                        <h3 className="text-bigText text-2xl font-bold">{item.title}</h3>
+                        <div>
+                        <p className="text-orange text-xl font-semibold">Price: ${item.price}</p>
+                        <button></button>
+                        </div>
+                    </div>)
+                }
+           </div>
         </section>
     );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import About from '../../Component/HomePageComponent/About/About';
 import Banner from '../../Component/HomePageComponent/Banner/Banner';
 import ContractInfo from '../../Component/HomePageComponent/ContractInfo/ContractInfo';
 import PopularProducts from '../../Component/HomePageComponent/PopularProducts/PopularProducts';
+import Service from '../../Component/HomePageComponent/Service/Service';
 import TeamSection from '../../Component/HomePageComponent/TeamSection/TeamSection';
 import Testimonial from '../../Component/HomePageComponent/Testimonial/Testimonial';
 import WhyChooseUs from '../../Component/HomePageComponent/WhyChooseUs/WhyChooseUs';
@@ -12,6 +13,7 @@ const Home = () => {
         
             <Banner/>
             <About/>
+            <Service/>
             <ContractInfo/>
             <PopularProducts/>
             <TeamSection/>
